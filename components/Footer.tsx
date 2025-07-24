3f647dddfcfce7bd9fed654e6bb22121d9289b94
iamanvar3 <iamanvar3@gmail.com>
2025-07-24 19:53:38 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,16 @@
 // components/Footer.tsx
-
 import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="mt-10 border-t pt-6 pb-10 text-center text-gray-600 text-sm">
+    <footer className="mt-10 border-t pt-6 pb-10 text-center text-gray-600 text-sm space-y-2">
       <p>© 2025 1-Page School. All rights reserved.</p>
-      <p className="mt-2">
+      <p>
         <Link href="/" className="text-blue-600 hover:underline">Home</Link> |{' '}
         <Link href="/" className="text-blue-600 hover:underline">Lessons</Link>
+      </p>
+      <p>
+        Built with ❤️ by mrNORD • <a href="https://github.com/meanvar3-5583" className="underline hover:text-black">GitHub</a>
       </p>
     </footer>
   );
