32a65dbaf4a31ed74c139264ceee92f58cf4413a
iamanvar3 <iamanvar3@gmail.com>
2025-07-24 20:24:42 +0530

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,8 +3,10 @@ import Footer from './Footer';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen flex flex-col justify-between">
-      <main className="flex-grow">{children}</main>
+    <div className="min-h-screen flex flex-col justify-between bg-white text-black">
+      <main className="flex-grow max-w-3xl w-full mx-auto px-4 py-10">
+        {children}
+      </main>
       <Footer />
     </div>
   );
