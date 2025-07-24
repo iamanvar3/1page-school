6ea71ec75aa474fe0bd75fcc7dd485e862a46924
iamanvar3 <iamanvar3@gmail.com>
2025-07-24 23:29:58 +0530

    Create next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://1page-school.vercel.app', // Or your custom domain later
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
