3b3cf5cedcc5323100ddfd33b2d582e45502a7d7
iamanvar3 <iamanvar3@gmail.com>
2025-07-25 21:00:19 +0530

    Create fonts.ts

---
+++ app/fonts.ts
@@ -0,0 +1,8 @@
+// app/fonts.ts
+import { Inter, Playfair_Display } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const playfair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
