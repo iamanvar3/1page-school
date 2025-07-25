c5f0d5514e6c4602d915d12e2cb0f94216b4fb7f
iamanvar3 <iamanvar3@gmail.com>
2025-07-25 22:05:33 +0530

    Update fonts.ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,13 +3,12 @@ import { Inter, Playfair_Display } from 'next/font/google';
 
 export const inter = Inter({
   subsets: ['latin'],
-  variable: '--font-inter', // 👈 this line enables `.variable`
+  variable: '--font-inter',
   display: 'swap',
 });
 
 export const playfair = Playfair_Display({
   subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-playfair', // 👈 this line enables `.variable`
+  variable: '--font-playfair',
   display: 'swap',
 });
