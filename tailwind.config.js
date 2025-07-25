d79ecd642f929516ede2689e5887ab5cac42ffa9
iamanvar3 <iamanvar3@gmail.com>
2025-07-25 21:41:47 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// tailwind.config.ts
+
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', 'sans-serif'],
+        serif: ['var(--font-playfair)', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
