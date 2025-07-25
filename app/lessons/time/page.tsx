44d8ebb3413b16e80f6c30aaafd263a9bae8836c
iamanvar3 <iamanvar3@gmail.com>
2025-07-25 22:55:58 +0530

    Update page.tsx

--- app/lessons/time/page.tsx
+++ app/lessons/time/page.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
-
+import Breadcrumb from '@/components/Breadcrumb';
 export default function TimeLesson() {
-  return (
+  return (<Breadcrumb />
     <main className="max-w-2xl mx-auto p-6 space-y-6">
       <h1 className="text-4xl font-bold mb-2">⏳ Manage Your Time</h1>
       <p className="text-lg text-gray-700">Spend it on what truly matters.</p>
