f3c12b2087caddde3b30ec1a174ffc29b6ed178e
iamanvar3 <iamanvar3@gmail.com>
2025-07-28 02:32:49 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://1page-school.vercel.app', // Or your custom domain later
+  siteUrl: 'https://1page-school.vercel.app', // Change later to your custom domain
   generateRobotsTxt: true,
   sitemapSize: 5000,
+  changefreq: 'weekly',
+  priority: 0.7,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    additionalSitemaps: [
+      'https://1page-school.vercel.app/sitemap.xml',
+    ],
+  },
 };
