2c9d854a1a0551a0d10be3514893614ef26eb67f
iamanvar3 <iamanvar3@gmail.com>
2025-07-30 21:19:17 +0530

    Create page.mdx.tsx

---
+++ app/lessons/life-skills/confidence/page.mdx.tsx
@@ -0,0 +1,6 @@
+// app/lessons/life-skills/confidence/page.mdx.tsx
+import MDXLayout from '@/components/MDXLayout';
+
+export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
+  return <MDXLayout>{children}</MDXLayout>;
+}
