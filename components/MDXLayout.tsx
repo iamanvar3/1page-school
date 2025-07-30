55da7b111668e6a5eab753396b4c5489860109f9
iamanvar3 <iamanvar3@gmail.com>
2025-07-30 21:01:57 +0530

    Update MDXLayout.tsx

--- components/MDXLayout.tsx
+++ components/MDXLayout.tsx
@@ -2,11 +2,14 @@
 'use client';
 
 import React from 'react';
+import SiteLayout from './SiteLayout'; // or './Layout' if not renamed
 
 export default function MDXLayout({ children }: { children: React.ReactNode }) {
   return (
-    <article className="prose dark:prose-invert prose-blue max-w-3xl mx-auto px-4 py-8">
-      {children}
-    </article>
+    <SiteLayout>
+      <article className="prose dark:prose-invert prose-blue max-w-3xl mx-auto px-4 py-8">
+        {children}
+      </article>
+    </SiteLayout>
   );
 }
