76cb46e6cbf4b7592e65f3f8fc32ea7ebedb39c9
iamanvar3 <iamanvar3@gmail.com>
2025-07-30 21:35:53 +0530

    Update layout.tsx

--- app/lessons/life-skills/confidence/layout.tsx
+++ app/lessons/life-skills/confidence/layout.tsx
@@ -1,6 +1,6 @@
-// app/lessons/life-skills/confidence/page.mdx.tsx
+// app/lessons/life-skills/confidence/layout.tsx
 import MDXLayout from '@/components/MDXLayout';
 
-export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return <MDXLayout>{children}</MDXLayout>;
 }
